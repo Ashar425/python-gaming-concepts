3fd3a7cfde072413305f5066ba49d8f43d0b8413
Ashar425 <qasimzafar80@gmail.com>
2025-09-05 17:05:06 +0200

    Add files via upload

--- cw.py
+++ cw.py
@@ -1,28 +1,64 @@
-matrix=[[1,2,3],[4,5,6],[7,8,9]]
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-print (matrix)
+print("Matrix:", matrix)
+print("Number of columns:", len(matrix[0]))
+print("Element at [1][2]:", matrix[1][2])
 
-print(len(matrix[0]))
+rows = len(matrix)
+columns = len(matrix[0])
 
-print(matrix[1][2])
+print("\nPrinting matrix elements:")
+for i in range(rows):
+    for j in range(columns):
+        print(matrix[i][j], end=" ")
+    print()
 
-rows=len(matrix)
-columns=len(matrix[0])
-for i in range(0,rows):
-    for j in range(0,columns):
-        print(matrix[i][j],end=" ")
-    print("\n")
+r = int(input("\nEnter the number of rows: "))
+c = int(input("Enter the number of columns: "))
 
-r=int(input("Enter the nuber of rows: "))
-c=int(input("Enter the nuber of columns: "))
-
-m=[]
-
+m = []
 for i in range(r):
-    temp=[]
+    temp = []
     for j in range(c):
-        x=int(input("Enter your element: "))
+        x = int(input(f"Enter element at [{i}][{j}]: "))
         temp.append(x)
-        m.append(temp)
+    m.append(temp)
 
-        
+print("\nYour entered matrix:")
+for i in range(r):
+    for j in range(c):
+        print(m[i][j], end=" ")
+    print()
+
+ro = int(input("\nEnter the number of rows (square matrix): "))
+col = ro
+
+ma = []
+for i in range(ro):
+    temp = []
+    for j in range(col):
+        x = int(input(f"Enter element at [{i}][{j}]: "))
+        temp.append(x)
+    ma.append(temp)
+
+print("\nSecond user matrix:")
+for i in range(ro):
+    for j in range(col):
+        print(ma[i][j], end=" ")
+    print()
+
+matrix1 = [[1, 2], [3, 4]]
+matrix2 = [[5, 6], [7, 8]]
+
+add = [[0, 0], [0, 0]]
+sub = [[0, 0], [0, 0]]
+
+for i in range(2):
+    for j in range(2):
+        add[i][j] = matrix1[i][j] + matrix2[i][j]
+        sub[i][j] = matrix1[i][j] - matrix2[i][j]
+
+print("\nMatrix 1:", matrix1)
+print("Matrix 2:", matrix2)
+print("Addition:", add)
+print("Subtraction:", sub)
